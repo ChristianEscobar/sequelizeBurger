1a5525ef538d04bfa15826de5004baab0f652426
Christian Escobar <crs.escobar@gmail.com>
2018-03-01 13:47:46 -0800

    updated route logic

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,36 +24,17 @@ router.get("/", (req, res) => {
 	});
 });
 
-/*
-router.get("/", (req, res) => {
-	burger.selectAll()
-		.then((result) => {
-			const burgersObj = {
-				burgers: result[0]
-			};
-			res.render("index", burgersObj);
-		}).catch((error) => {
-			console.error(error);
-		});
-});
-
-router.post("/", (req, res) => {
-	burger.insertOne(req.body.burgerDescription, false)
-		.then((result) => {
-			res.redirect("/");
-		}).catch((error) => {
-			console.error(error);
-		});
-});
-
+// Update devoured flag for a specific burger based on the id
 router.put("/:id", (req, res) => {
-	burger.updateOne(req.params.id)
-		.then((result) => {
-			res.redirect("/");
-		}).catch((error) => {
-			console.error(error);
-		});
+	Burger.findById(req.params.id)
+	.then((burger) => {
+		burger.update(req.body)
+		.then((results) => {
+			res.status(200).json(results);
+		})
+	}).catch((error) => {
+		res.status(500).send(error);
+	})
 });
-*/
 
 module.exports = router;
